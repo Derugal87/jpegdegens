cfd2b056845135085385d6b705ec82d7efd56782
Alexander Derugo <alexderugo@gmail.com>
2023-03-08 03:48:06 +0400

    final

---
+++ test/Hero.ts
@@ -0,0 +1,50 @@
+import "@nomiclabs/hardhat-ethers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+
+describe("Hero", function () {
+	async function createHero() {
+		const Hero = await ethers.getContractFactory("TestHero");
+		const hero = await Hero.deploy();
+		await hero.deployed();
+
+		return hero;
+	}
+
+	let hero;
+
+	before(async function () {
+		hero = await createHero();
+	});
+
+	it("should get a zero hero array", async function () {
+		expect(await hero.getHeroes()).to.deep.equal([]);
+	});
+
+	it("should fail at creating hero cause of payment", async function () {
+		let e;
+
+		try {
+			await hero.createHero(0, {
+				value: ethers.utils.parseEther("0.04999999")
+			});
+		} catch (error) {
+			e = error;
+		}
+		expect(e.message.includes("Please send more money")).to.equal(true);
+	});
+
+	it("should fail at creating hero cause of payment", async function () {
+		const hero = await createHero();
+
+		await hero.setRandom(69);
+		await hero.createHero(0, {
+			value: ethers.utils.parseEther("0.05")
+		});
+		const h = (await hero.getHeroes())[0];
+
+		expect(await hero.getMagic(h)).to.equal(16);
+		expect(await hero.getHealth(h)).to.equal(2);
+	});
+});
